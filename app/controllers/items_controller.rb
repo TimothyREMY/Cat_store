4f7d52cded2495ae8751f7a7b894c4e2c76a5aa7
ThoMich22 <thomas_8_69@hotmail.com>
2021-06-07 17:30:45 +0200

    add Methode Controller Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,21 @@
 class ItemsController < ApplicationController
+  
+  def new 
+    @item = Item.new
+  end
+
+  def index
+    @items = Item.all 
+  end
+
+  def create
+    @item = Item.create(item_params) 
+  end
+
+  private 
+
+  def item_params
+    params.permit(:title, :description, :price, :image_url)
+  end
+  
 end
