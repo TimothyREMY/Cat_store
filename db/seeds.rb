f1a5070a1fcea0226c6c83cfdc482a9b1ddc8e66
TimothyREMY <timothy.remy@gmail.com>
2021-06-07 20:35:41 +0200

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,11 @@
 #Cart.delete_all
 #Order.delete_all
 #Selection.delete_all
-Item.delete_all
+#Item.delete_all
 
-20.times do 
-  Item.create(title: ["Doudou", "Chouchou", "Poupou", "Loulou", "Minou", "Pikachu", "Patapon", "Grosminet", "Popeye", "Berlioz", "Billy", "Azraël", "Othello", "Toulouse", "O’Malley", "Figaro", "Oliver", "Felix", "Lucifer", "Olympe"].sample, description: Faker::Creature::Cat.breed, price: rand(10..1000), image_url: "https://images.pexels.com/photos/2173872/pexels-photo-2173872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+20.times do |i|
+  Item.create(title: ["Doudou", "Chouchou", "Poupou", "Loulou", "Minou", "Pikachu", "Patapon", "Grosminet", "Popeye", "Berlioz", "Billy", "Azraël", "Othello", "Toulouse", "O’Malley", "Figaro", "Oliver", "Felix", "Lucifer", "Olympe"].sample, description: Faker::Lorem.characters(number: 35), price: rand(10..1000), image_url: "https://images.pexels.com/photos/2173872/pexels-photo-2173872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+  puts "Item number #{i} a été créé"
 end
 
 =begin
