a5a79a6b0d50c6801a56f9b3611bfc99a80fc1e7
TimothyREMY <timothy.remy@gmail.com>
2021-06-08 19:26:00 +0200

    Back show cart

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -19,15 +19,13 @@ class SelectionsController < ApplicationController
     end
   end
 
-  # OU ALORS si @current_user.cart ne fonctionne pas :
-      # quand user se connecte => création d'un nouveau panier
-      # >>>>> dernière entrée de la BDD "Cart" == panier actuel
-      # current_panier = Panier.last
-
-  # private 
-
-  # def selection_params
-  #   params.permit(:item_id)
-  # end
+  def destroy
+    @cart = Cart.find(current_user.id)
+    @selection = Selection.find(params[:id])
+    @selection.destroy
+    flash[:success] = "Photo supprimée au panier!"
+      puts "Succès ! #{@selection.item_id} a été supprimé du panier #{Cart.find(@current_user.id)}"
+      redirect_to cart_path(@cart.id)
+  end
 
 end
