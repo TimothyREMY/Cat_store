0fe0451b7c4598b1b1698f0c445dac14914fc340
ThoMich22 <thomas_8_69@hotmail.com>
2021-06-08 11:58:37 +0200

    second modif

--- db/migrate/20210608080948_add_cart_id_to_user.rb
+++ db/migrate/20210608080948_add_cart_id_to_user.rb
@@ -1,7 +1,7 @@
 class AddCartIdToUser < ActiveRecord::Migration[5.2]
   def change
 
-    add_reference :user, :cart_id, foreign_key :true 
+    add_reference :users, :cart, foreign_key: true 
     
   end
 end
